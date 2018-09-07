880dd78cd1ad258fd14e6404419d684a6df4f149
Michael Rich <mtrich@satx.rr.com>
2018-09-07 18:46:13 -0500

    tic-tac-toe final

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,83 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
-});
+  var move = 'X';
+  $('[data-cell]').on('click', function(){
+    if($(this).text() === ''){
+    $(this).text(move);
+    win();
+    if (move === 'X') {
+      move = 'O';
+    }else {
+      move = 'X';
+    }
+    }
+ });
+
+  function win(){
+    if
+      ($('[data-cell = "0"]').text() === move &&
+       $('[data-cell = "3"]').text() === move &&
+       $('[data-cell = "6"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+     } else if
+      ($('[data-cell = "1"]').text() === move &&
+       $('[data-cell = "4"]').text() === move &&
+       $('[data-cell = "7"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "2"]').text() === move &&
+       $('[data-cell = "5"]').text() === move &&
+       $('[data-cell = "8"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "0"]').text() === move &&
+       $('[data-cell = "1"]').text() === move &&
+       $('[data-cell = "2"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "0"]').text() === move &&
+       $('[data-cell = "1"]').text() === move &&
+       $('[data-cell = "2"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "3"]').text() === move &&
+       $('[data-cell = "4"]').text() === move &&
+       $('[data-cell = "5"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "6"]').text() === move &&
+       $('[data-cell = "7"]').text() === move &&
+       $('[data-cell = "8"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "2"]').text() === move &&
+       $('[data-cell = "4"]').text() === move &&
+       $('[data-cell = "6"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "0"]').text() === move &&
+       $('[data-cell = "4"]').text() === move &&
+       $('[data-cell = "8"]').text() === move){
+       $('#announce-winner').text(`${move} wins!`);
+    } else if
+      ($('[data-cell = "0"]').text() !== '' &&
+       $('[data-cell = "1"]').text() !== '' &&
+       $('[data-cell = "2"]').text() !== '' &&
+       $('[data-cell = "3"]').text() !== '' &&
+       $('[data-cell = "4"]').text() !== '' &&
+       $('[data-cell = "5"]').text() !== '' &&
+       $('[data-cell = "6"]').text() !== '' &&
+       $('[data-cell = "7"]').text() !== '' &&
+       $('[data-cell = "8"]').text() !== ''){
+       $('#announce-winner').text(`It's a Tie!`);
+      };
+
+  }
+
+  $('#clear').click(function(){
+    $('[data-cell]').empty();
+    move = "X";
+    $('#announce-winner').empty();
+  });
+})
